ef11affac94dcc03e0f64872962fcab409af9acf
Rauljp16 <raul_astra16@hotmail.com>
2024-07-03 19:22:33 +0200

    testing terminado

--- test/index.js
+++ test/index.js
@@ -13,36 +13,44 @@ class Room {
   }
 
   occupancyPercentage(startDate, endDate) {
-    const totalDays = (endDate - startDate) / (1000 * 60 * 60 * 24); // Convertir a días
-    let occupiedDays = 0;
-
-    this.bookings.forEach((booking) => {
-      const bookingStart =
-        booking.checkIn < startDate ? startDate : booking.checkIn;
-      const bookingEnd =
-        booking.checkOut > endDate ? endDate : booking.checkOut;
-
-      if (bookingStart < endDate && bookingEnd > startDate) {
-        occupiedDays += (bookingEnd - bookingStart) / (1000 * 60 * 60 * 24); // Convertir a días
+    let occupancyDays = 0;
+    let totalDays = endDate - startDate;
+    totalDays = totalDays / (24 * 60 * 60 * 1000);
+    for (
+      let date = new Date(startDate.getTime());
+      date < endDate;
+      date.setDate(date.getDate() + 1)
+    ) {
+      if (this.isOccupied(date)) {
+        occupancyDays++;
       }
-    });
-
-    if (totalDays === 0) return 0;
-    return (occupiedDays / totalDays) * 100;
+    }
+    let percentage = (occupancyDays / totalDays) * 100;
+    percentage = Math.round(percentage);
+    return percentage;
   }
 
   static totalOccupancyPercentage(rooms, startDate, endDate) {
-    const totalPercentages = rooms.reduce(
-      (acc, room) => acc + room.occupancyPercentage(startDate, endDate),
-      0
-    );
-    return totalPercentages / rooms.length;
+    let totalPercentage = 0;
+    rooms.forEach((element) => {
+      totalPercentage += element.occupancyPercentage(startDate, endDate);
+    });
+    // for (let index = 0; index < rooms.length; index++) {
+    //   const element = rooms[index];
+    //   totalPercentage += element.occupancyPercentage(startDate, endDate);
+    // }
+    totalPercentage = totalPercentage / rooms.length;
+    return totalPercentage;
   }
 
   static availableRooms(rooms, startDate, endDate) {
-    return rooms.filter(
-      (room) => !room.isOccupied(startDate) && !room.isOccupied(endDate)
-    );
+    const availableRooms = [];
+    for (let index = 0; index < rooms.length; index++) {
+      if (rooms[index].occupancyPercentage(startDate, endDate) === 0)
+        availableRooms.push(rooms[index]);
+    }
+
+    return availableRooms;
   }
 }
 
@@ -56,7 +64,15 @@ class Booking {
     this.room = object.room;
   }
 
-  get fee() {}
+  getFee() {
+    let totalDays = this.checkOut - this.checkIn;
+    totalDays = totalDays / (24 * 60 * 60 * 1000);
+    let totalRate = this.room.rate * totalDays;
+    totalRate = totalRate - (totalRate * this.discount) / 100;
+    totalRate = totalRate - (totalRate * this.room.discount) / 100;
+    console.log(totalRate);
+    return totalRate;
+  }
 }
 
 module.exports = { Room, Booking };
--- test/index.test.js
+++ test/index.test.js
@@ -65,22 +65,158 @@ describe("Clase Room", () => {
       room.bookings = [booking1, booking2];
 
       expect(
-        room.occupancyPercentage(new Date("2024-01-01"), new Date("2024-05-01"))
-      ).toBeCloseTo(8, 121);
+        room.occupancyPercentage(new Date("2024-01-10"), new Date("2024-01-13"))
+      ).toBe(100);
     });
 
     test("occupancy percentage when there are no reservations", () => {
       const room = new Room({ ...roomTemplate });
       const booking1 = new Booking({
         ...bookingTemplate,
+        checkIn: new Date("2024-01-10"),
+        checkOut: new Date("2024-01-17"),
       });
       const booking2 = new Booking({
         ...bookingTemplate,
+        checkIn: new Date("2024-02-10"),
+        checkOut: new Date("2024-02-17"),
       });
       room.bookings = [booking1, booking2];
       expect(
         room.occupancyPercentage(new Date("2024-01-01"), new Date("2024-05-01"))
-      ).toBe(0);
+      ).toBe(12);
+    });
+  });
+  describe("Metod occupancy total percentage", () => {
+    test("Total occupancy percentage in all rooms", () => {
+      const room = new Room({ ...roomTemplate });
+      const booking1 = new Booking({
+        ...bookingTemplate,
+        checkIn: new Date("2024-01-10"),
+        checkOut: new Date("2024-01-17"),
+      });
+      const booking2 = new Booking({
+        ...bookingTemplate,
+        checkIn: new Date("2024-02-10"),
+        checkOut: new Date("2024-02-17"),
+      });
+      room.bookings = [booking1, booking2];
+      const room1 = new Room({
+        ...roomTemplate,
+        bookings: [booking1, booking2],
+      });
+      const room2 = new Room({
+        ...roomTemplate,
+        bookings: [booking1, booking2],
+      });
+
+      const listRooms = [room1, room2];
+
+      expect(
+        Room.totalOccupancyPercentage(
+          listRooms,
+          new Date("2024-01-01"),
+          new Date("2024-05-01")
+        )
+      ).toBe(12);
+    });
+  });
+  describe("Metod available rooms", () => {
+    test("rooms that are not occupied", () => {
+      const booking1 = new Booking({
+        ...bookingTemplate,
+        checkIn: new Date("2024-01-10"),
+        checkOut: new Date("2024-01-13"),
+      });
+      const booking2 = new Booking({
+        ...bookingTemplate,
+        checkIn: new Date("2024-02-15"),
+        checkOut: new Date("2024-02-17"),
+      });
+
+      const room1 = new Room({
+        ...roomTemplate,
+        name: "luxury single bed",
+        bookings: [booking1],
+      });
+      const room2 = new Room({
+        ...roomTemplate,
+        bookings: [booking2],
+      });
+
+      const listRooms = [room1, room2];
+
+      expect(
+        Room.availableRooms(
+          listRooms,
+          new Date("2024-02-01"),
+          new Date("2024-02-25")
+        )
+      ).toEqual([room1]);
+    });
+  });
+});
+describe("Clase Booking", () => {
+  describe("Método getFee", () => {
+    test("price without discounts", () => {
+      const booking = new Booking({
+        ...bookingTemplate,
+        checkIn: new Date("2024-01-10"),
+        checkOut: new Date("2024-01-13"),
+      });
+
+      const room1 = new Room({
+        ...roomTemplate,
+        bookings: [booking],
+      });
+      booking.room = room1;
+      expect(booking.getFee()).toEqual(30000);
+    });
+    test("discounted price in room", () => {
+      const booking = new Booking({
+        ...bookingTemplate,
+        checkIn: new Date("2024-01-10"),
+        checkOut: new Date("2024-01-13"),
+      });
+
+      const room1 = new Room({
+        ...roomTemplate,
+        bookings: [booking],
+        discount: 30,
+      });
+      booking.room = room1;
+      expect(booking.getFee()).toEqual(21000);
+    });
+    test("discounted price in booking", () => {
+      const booking = new Booking({
+        ...bookingTemplate,
+        discount: 10,
+        checkIn: new Date("2024-01-10"),
+        checkOut: new Date("2024-01-13"),
+      });
+
+      const room1 = new Room({
+        ...roomTemplate,
+        bookings: [booking],
+      });
+      booking.room = room1;
+      expect(booking.getFee()).toEqual(27000);
+    });
+    test("discounted price in booking and room", () => {
+      const booking = new Booking({
+        ...bookingTemplate,
+        discount: 10,
+        checkIn: new Date("2024-01-10"),
+        checkOut: new Date("2024-01-13"),
+      });
+
+      const room1 = new Room({
+        ...roomTemplate,
+        discount: 10,
+        bookings: [booking],
+      });
+      booking.room = room1;
+      expect(booking.getFee()).toEqual(24300);
     });
   });
 });
